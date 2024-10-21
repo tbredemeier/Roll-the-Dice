b07ccfdd4541409783d0cf841340462535a00082
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-21 13:33:50 -0500

    Added dice rolling effect

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
             Text("Roll Dice").font(.title)
             Image("pips \(randomValue)")
                 .resizable()
                 .frame(width: 200, height: 200)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                 .padding()
                 .onTapGesture {
                     randomValue = Int.random(in: 1...6)
+                    withAnimation {
+                        rotation += 360
+                    }
                 }
             Spacer()
         }
