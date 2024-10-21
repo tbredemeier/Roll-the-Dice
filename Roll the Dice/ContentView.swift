a035c0af797bf44397cf73a0cc5af82136a0e350
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-21 13:29:50 -0500

    Added die images

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -12,7 +12,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Roll Dice").font(.title)
-            Text("\(randomValue)").font(.system(size: 72))
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200)
                 .padding()
                 .onTapGesture {
                     randomValue = Int.random(in: 1...6)
