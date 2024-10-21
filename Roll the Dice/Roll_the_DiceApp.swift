437a1111049fc378e2a3903d14a04e30f38c0ddc
Tom Bredemeier <tbredemeier@barrington220.org>
2024-10-21 10:18:40 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Tom Bredemeier on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
